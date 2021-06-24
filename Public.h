a42bf9d9e27c1bfa972f01a174664762b0e8b363
Jeff Kubascik <jeffkub@gmail.com>
2021-06-24 00:35:04 -0400

    Re-organizing code

--- Public.h
+++ Public.h
@@ -1,23 +1,10 @@
-/*++
+#pragma once
 
-Module Name:
+#include <ntddk.h>
+#include <wdf.h>
+#include <initguid.h>
 
-    public.h
-
-Abstract:
-
-    This module contains the common declarations shared by driver
-    and user applications.
-
-Environment:
-
-    user and kernel
-
---*/
-
-//
-// Define an Interface Guid so that apps can find the device and talk to it.
-//
+#include "trace.h"
 
 DEFINE_GUID (GUID_DEVINTERFACE_tw68xx,
     0x8cacffba,0xfc26,0x45a0,0xa6,0x10,0xf1,0x92,0x24,0x85,0x21,0x90);
